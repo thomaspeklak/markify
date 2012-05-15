819efd08d3834b1257b1472f508cc3d7ebba7083
Thomas Peklak <thomas.peklak@gmail.com>
2012-05-15 21:56:49 +0200

    initial commit

---
+++ src/markify/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns markify.views.welcome
+  (:require [markify.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to markify"]))
