687af5a05cb90f67e5ff2235708e4ca6e01d5e19
Thomas Peklak <thomas.peklak@gmail.com>
2012-05-16 23:00:03 +0200

    added text and links and register page

--- src/markify/views/marks.clj
+++ src/markify/views/marks.clj
@@ -2,10 +2,22 @@
   (:require [markify.views.common :as common]
             [noir.response :as resp])
   (:use [noir.core :only [defpage]]
-        [hiccup.core :only [html]]
+        [hiccup.core ]
         [hiccup.page-helpers]))
 
 (defpage "/marks" []
          (common/layout
-           [:p "On your marks, ready, set, go"]))
+           [:h1 "Markify"]
+           [:p "On your marks, ready, set, go"]
+           [:div.description
+            [:p 
+              "Markify is an easy and fast bookmarking tool. "
+              (link-to "/register" "Set up") 
+              " an account and you are ready to start."]]))
 
+(defpage [:get "/register"] []
+  (common/layout
+    [:h1 "Register"]
+
+  )
+)
