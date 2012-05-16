e3d963a8bb80d2176da7af8c9ed6de84b128d25f
Thomas Peklak <thomas.peklak@gmail.com>
2012-05-16 23:30:56 +0200

    added signup form

--- src/markify/views/marks.clj
+++ src/markify/views/marks.clj
@@ -3,6 +3,7 @@
             [noir.response :as resp])
   (:use [noir.core :only [defpage]]
         [hiccup.core ]
+        [hiccup.form-helpers ]
         [hiccup.page-helpers]))
 
 (defpage "/marks" []
@@ -15,9 +16,10 @@
               (link-to "/register" "Set up") 
               " an account and you are ready to start."]]))
 
-(defpage [:get "/register"] []
+(defpage "/register" {:as user}
   (common/layout
     [:h1 "Register"]
-
-  )
-)
+    (form-to [:post "/register"]
+      [:p [:label "Username:" (text-field "username") ]]
+      [:p [:label "Password:" (password-field "password") ]]
+      (submit-button "Mark my words"))))
